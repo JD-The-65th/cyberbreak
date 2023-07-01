28f399bd23fc7bc843c88a68582eb4405e2cab09
JD Williams <jdwill239@gmail.com>
2023-07-01 00:30:58 -0400

    Remove android Build Specific code ONE LAST TIME

--- android/build/build/intermediates/assets/debug/mergeDebugAssets/addons/xr-controller-midi-player/plugin.gd
+++
@@ -1,8 +0,0 @@
-@tool
-extends EditorPlugin
-
-func _enter_tree():
-	add_custom_type("XRControllerMidiPlayer", "Node", preload("xr_controller_midi_player.gd"), preload("icon.png"))
-
-func _exit_tree():
-	remove_custom_type("XRControllerMidiPlayer")
