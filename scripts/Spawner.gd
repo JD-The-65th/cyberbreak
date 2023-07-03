ae5e1c5e7264657eecf7fd69a7001efe8bf93b5e
JD Williams <jdwill239@gmail.com>
2023-07-03 04:18:05 -0400

    Many Changes (I don't remember how many)

--- scripts/Spawner.gd
+++ scripts/Spawner.gd
@@ -4,7 +4,28 @@ extends MeshInstance3D
 
 
 
+func _ready():
+	for i in self.get_children():
+		if i.get_class() == "RigidBody3D":
+			i.freeze = true
+			i.set_gravity_scale(0.0)
+			var attachmentPoint : Generic6DOFJoint3D = Generic6DOFJoint3D.new()
+			attachmentPoint.set_transform(i.transform)
+			attachmentPoint.node_a = get_path_to($StaticBody3D)
+			attachmentPoint.node_b = get_path_to(i)
+			i.set_meta("attachmentPoint", attachmentPoint)
+			i.set_meta("attachedToSpawner", true)
+			i.freeze = false
+			
+
+
+
 
 func _on_area_3d_body_exited(body: Node3D):
-	body.set_collision_layer(pow(2, 1-3))
-	body.reparent(root_node)
+	if body.get_meta("attachedToSpawner"):
+		var attachmentPoint = body.get_meta("attachmentPoint")
+		attachmentPoint.queue_free()
+		body.set_meta("attachedToSpawner", false)
+		body.set_gravity_scale(1.0)
+		body.set_collision_layer(pow(2, 3-1))
+		body.reparent(root_node)
