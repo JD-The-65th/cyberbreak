a0b551bc6d28f047fa04c7158e828076ebe39390
JD Williams <jdwill239@gmail.com>
2023-07-09 06:51:26 -0400

    w- wow.

--- components/state_machine_component.gd
+++ components/state_machine_component.gd
@@ -24,7 +24,7 @@ func _process(delta):
 
 func _physics_process(delta):
 	if current_state:
-		current_state.physics_pdate(delta)
+		current_state.physics_update(delta)
 
 func on_child_transition(state, new_state_name):
 	if state != current_state:
