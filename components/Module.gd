ee0b54bec20a9a2b575ed218bfb1b90afb0c42b8
JD Williams <jdwill239@gmail.com>
2023-07-12 02:05:55 -0400

    Functional. Building.

--- components/Module.gd
+++ components/Module.gd
@@ -6,3 +6,14 @@ signal unsnapped
 
 signal scaled(new_scale: Vector3)
 
+
+func spawn_joint(type: String = "Generic") -> Joint3D: ## Spawns a Physics Joint as a sibling and returns it.
+	var joint = Joint3D
+	if type == "Socket":
+		joint = ConeTwistJoint3D.new()
+	else:
+		joint = Generic6DOFJoint3D.new()
+	
+	add_sibling(joint)
+	return joint
+	
--- scripts/SnapZone.gd
+++ scripts/SnapZone.gd
@@ -3,29 +3,16 @@ extends Area3D
 var closest_module: RigidBody3D
 var snapped_module: RigidBody3D
 
-func _ready():
-	# Disable Location Tracking
-	get_parent().set_flag_x(0, false)
-	get_parent().set_flag_y(0, false)
-	get_parent().set_flag_z(0, false)
-	# Disable Rotation Tracking
-	get_parent().set_flag_x(1, false)
-	get_parent().set_flag_y(1, false)
-	get_parent().set_flag_z(1, false)
+var joint : Joint3D
 
 func snap_module(module: RigidBody3D):
-	#No sanity checking yet, jus gonna assume parent is a Joint
-	get_parent().node_b = get_path_to(module)
-	# Location Tracking
-	get_parent().set_flag_x(0, true)
-	get_parent().set_flag_y(0, true)
-	get_parent().set_flag_z(0, true)
-	# Rotation Tracking
-	get_parent().set_flag_x(1, true)
-	get_parent().set_flag_y(1, true)
-	get_parent().set_flag_z(1, true)
-	
+	#No sanity checking yet, jus gonna assume parent is a Module cuz why wouldn't it be
+	joint = get_parent().spawn_joint()
+	$RemoteTransform3D.remote_path = joint.get_path() 
+	joint.node_a = get_parent().get_path()
+	joint.node_b = module.get_path()
 	snapped_module = module
+	
 
 
 func _on_body_entered(body: RigidBody3D):
