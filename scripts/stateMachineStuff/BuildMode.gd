0e56b26cc81245d56854c4ee06443341ab6ba5aa
JD Williams <jdwill239@gmail.com>
2023-07-12 13:07:10 -0400

    Delay Build Mode switching

--- scripts/stateMachineStuff/BuildMode.gd
+++ scripts/stateMachineStuff/BuildMode.gd
@@ -4,6 +4,8 @@ class_name BuildMode
 # I'm calling it now
 # A lot of this code is most likely redundant and can probably be done easier
 # If an easier solution IS found, slap me.
+
+
 @export var left_grab_function : GrabFunction
 @export var right_grab_function : GrabFunction
 @export var settings_controller : XRController3D
@@ -12,13 +14,13 @@ func enter():
 	$"../../XRCamera3D/MeshInstance3D".visible = true
 	left_grab_function.snappable = true
 	right_grab_function.snappable = true
-	await get_tree().create_timer(0.4).timeout
+	await get_tree().create_timer(1).timeout
 
 func exit():
 	$"../../XRCamera3D/MeshInstance3D".visible = false
 	left_grab_function.snappable = false
 	right_grab_function.snappable = false
-	await get_tree().create_timer(0.4).timeout
+	await get_tree().create_timer(1).timeout
 	
 func update(delta: float):
 	if settings_controller.is_button_pressed("ax_button"):
